78b87f910fc0fa36a307fb3dff8c12f954182e21
Pedro Ap <pedroaparecidori@gmail.com>
2024-01-30 18:13:50 -0300

    Criado controllers para user e post

---
+++ src/Controller/postController.js
@@ -0,0 +1,13 @@
+import { Router } from "express"
+
+const router = Router()
+
+router.post('/', (req, res) => {
+    res.send('CREATE POST /')
+})
+
+router.get('/:id?', (req, res) => {
+    res.send('GET POST /')
+})
+
+export default router
