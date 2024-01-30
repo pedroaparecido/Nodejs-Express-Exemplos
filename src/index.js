1cb0260a54a4364c04ba9be83c445f330adb10ec
Pedro Ap <pedroaparecidori@gmail.com>
2024-01-30 16:21:35 -0300

    Criado o index e instalado express

---
+++ src/index.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('Hello World 2')
+})
+
+app.listen(3000, () => {console.log('ONLINE http://localhost:3000')})
