9e8c8c5ff2eb9c43317e19bad6660f737a1b6255
Pedro Ap <pedroaparecidori@gmail.com>
2024-01-30 19:02:19 -0300

    Criado tratativa de erro para erros para signup

--- src/Modules/user/userService.js
+++ src/Modules/user/userService.js
@@ -3,9 +3,8 @@ let users = []
 const getUserByEmail = (searchEmail) => users.find((obj) => obj.email === searchEmail)
 
 export const signup = (data) => {
-    if (getUserByEmail(data.email)) {
-        console.log('EXISTE O EMAIL')
-    }
+    if (getUserByEmail(data.email)) throw new Error('email_existente')
+    
     users.push(data)
     return true
 }
